0a53e1f1290e1e8740f9d5693ad63557b91b41a0
Wesley Maa <wesley.maa@gmail.com>
2024-12-15 19:14:54 -0800

    build flags

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -961,9 +961,9 @@ uv-new() {
     source ${ENV_PATH}/bin/activate
 }
 
-# Use UV instead of regular PIP.
-alias pip='uv pip'
-alias pip3='uv pip'
+# # Use UV instead of regular PIP.
+# alias pip='uv pip'
+# alias pip3='uv pip'
 
 # ---------------
 # Tools for Slurm
